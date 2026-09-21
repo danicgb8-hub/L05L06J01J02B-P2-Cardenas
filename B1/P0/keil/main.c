6aa802701e4d08459196aec714dfc76fb8700d27
Daniel <danicgb8@gmail.com>
2026-09-21 19:47:57 +0200

    Realiza ejercicio 3 de P0, invierte estado logico de pines PB0 y PB7

--- B1/P0/keil/main.c
+++ B1/P0/keil/main.c
@@ -105,6 +105,16 @@ int main(void)
 
   /* Add your application code here*/
   __HAL_RCC_GPIOB_CLK_ENABLE () ;
+  
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 #ifdef RTE_CMSIS_RTOS2
   /* Initialize CMSIS-RTOS2 */
@@ -120,6 +130,10 @@ int main(void)
   /* Infinite loop */
   while (1)
   {
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+    HAL_Delay(100);
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
+    HAL_Delay(100);
   }
 }
 
